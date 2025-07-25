1b0df3a09ca0120931ee91c9ac5802116774600b
lumi <1uuuuuuuuuuuuuuumi@gmail.com>
2025-07-25 18:22:31 +0900

    바깥쪽 반복문 종료

--- Java/chapter04/BreakExample.java
+++ Java/chapter04/BreakExample.java
@@ -2,7 +2,7 @@ package chapter04;
 
 public class BreakExample {
     public static void main(String[] args) {
-        while (true){
+         while (true){
             int num = (int) (Math.random() * 6) + 1;
             System.out.println(num);
             if(num == 6){

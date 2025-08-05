5b33ddaca121afef4069ddcf4db4aeb40e6d2da8
lumi <1uuuuuuuuuuuuuuumi@gmail.com>
2025-08-05 22:58:33 +0900

    필드 자동 초기화

---
+++ Java/chapter06/FieldInitValue.java
@@ -0,0 +1,17 @@
+package chapter06;
+
+public class FieldInitValue {
+    //필드
+    byte byteField;
+    short shortField;
+    int intfield;
+    long longField;
+
+    boolean booleanField;
+    char charField;
+
+    float floatField;
+    double doubleField;
+    int[] arrField;
+    String referenceField;
+}

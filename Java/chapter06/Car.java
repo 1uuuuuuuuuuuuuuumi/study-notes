abd885064b3ac90b3f0041f3da8762e6a554338d
lumi <1uuuuuuuuuuuuuuumi@gmail.com>
2025-08-10 09:42:11 +0900

    생성자의 오버로딩

--- Java/chapter06/Car.java
+++ Java/chapter06/Car.java
@@ -9,7 +9,22 @@ public class Car {
     int speed;
 
     //생성자
-    Car(String color, int cc){
+    Car(){
 
+    }
+
+    Car(String model){
+        this.model = model;
+    }
+
+    Car(String model, String color){
+        this.model = model;
+        this.color = color;
+    }
+
+    Car(String model, String color, int maxSpeed){
+        this.model = model;
+        this.color = color;
+        this.maxSpeed = maxSpeed;
     }
 }

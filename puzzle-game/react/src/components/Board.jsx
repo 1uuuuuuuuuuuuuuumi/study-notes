8a9178623c2383f5fc82a9b0fa7aa9cfbbd2ecc0
lumi <1uuuuuuuuuuuuuuumi@gmail.com>
2025-11-15 21:30:38 +0900

    게임판(Board) 컴포넌트 작성

---
+++ puzzle-game/react/src/components/Board.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import '../styles/Board.css';
+
+function Board(){
+
+  return (
+    <div className="game-container">
+      <h1>Puzzle Game</h1>
+      <div className="board">
+        {/* 여기에 타일들이 들어갈 예정 */}
+        <div className="title">1</div>
+        <div className="title">2</div>
+        <div className="title">3</div>
+        <div className="title">4</div>
+        <div className="title">5</div>
+        <div className="title">6</div>
+        <div className="title">7</div>
+        <div className="title">8</div>
+        <div className="title">9</div>
+        <div className="title">10</div>
+        <div className="title">11</div>
+        <div className="title">12</div>
+        <div className="title">13</div>
+        <div className="title">14</div>
+        <div className="title">15</div>
+        <div className="title empty"></div>
+      </div>
+    </div>
+  );
+}
+
+export default Board;

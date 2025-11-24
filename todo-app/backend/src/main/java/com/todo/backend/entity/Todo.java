560b592dda77b97917f1481113ea25a1c532eda8
lumi <1uuuuuuuuuuuuuuumi@gmail.com>
2025-11-25 01:11:07 +0900

    Todo Entity 설계 작성중

---
+++ todo-app/backend/src/main/java/com/todo/backend/entity/Todo.java
@@ -0,0 +1,14 @@
+package com.todo.backend.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "todos")
+public class Todo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    
+}

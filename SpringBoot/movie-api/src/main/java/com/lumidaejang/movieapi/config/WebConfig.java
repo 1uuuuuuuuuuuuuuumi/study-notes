1285171c563ac032cac4e22510c9d294fdf61ab9
lumi <1uuuuuuuuuuuuuuumi@gmail.com>
2026-01-06 17:52:03 +0900

    CORS 설정

--- SpringBoot/movie-api/src/main/java/com/lumidaejang/movieapi/config/WebConfig.java
+++ SpringBoot/movie-api/src/main/java/com/lumidaejang/movieapi/config/WebConfig.java
@@ -1,4 +1,19 @@
 package com.lumidaejang.movieapi.config;
 
-public class WebConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/api/**")  // /api로 시작하는 모든 경로
+                .allowedOrigins("http://localhost:5173")    // React 주소
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용 메서드
+                .allowedHeaders("*")    // 모든 헤더 허용
+                .allowCredentials(true);    // 쿠키 허용
+    }
+
 }

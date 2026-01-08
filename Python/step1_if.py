67705608301046bca71bd24dda12882fd0af95f0
lumi <1uuuuuuuuuuuuuuumi@gmail.com>
2026-01-08 21:14:00 +0900

    파이썬 if 복습

--- Python/step1_if.py
+++ Python/step1_if.py
@@ -16,3 +16,11 @@ name = "호빵맨"
 age = 11
 
 print("안녕, 나는", name, "이고 나이는", age, "살이야!")
+
+# 2026.1.8 21:13 복습
+age = int(input("나이 입력: "))
+
+if age >= 20:
+  print("성인입니다")
+else:
+  print("미성년자입니다")
